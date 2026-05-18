0a553b90ed641b9e3136c77a1b29c6018efa9381
Your Name <luhungadi571@gmail.com>
2026-05-18 12:58:34 +0700

    Jobsheet 13

--- PraktikumPWL/app/Filament/Resources/Posts/Tables/PostsTable.php
+++ PraktikumPWL/app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -12,76 +12,56 @@ use Filament\Tables\Filters\SelectFilter;
 use Filament\Forms\Components\DatePicker;
 use Illuminate\Database\Eloquent\Builder;
 
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\ReplicateAction;
+use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Checkbox;
+
 class PostsTable
 {
     public static function configure(Table $table): Table
     {
         return $table
             ->columns([
-                TextColumn::make('id')
-                    ->label('ID')
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-
-                TextColumn::make('title')
-                    ->label('Title')
-                    ->sortable()
-                    ->searchable()
-                    ->toggleable(),
-
-                TextColumn::make('slug')
-                    ->label('Slug')
-                    ->sortable()
-                    ->searchable()
-                    ->toggleable(),
-
-                TextColumn::make('category.name')
-                    ->label('Category')
-                    ->sortable()
-                    ->searchable()
-                    ->toggleable(),
-
-                ColorColumn::make('color')
-                    ->toggleable(),
-
-                ImageColumn::make('image')
-                    ->disk('public')
-                    ->toggleable(),
-
-                TextColumn::make('created_at')
-                    ->label('Created At')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(),
-                
-                TextColumn::make('tags')
-                    ->label('Tags')
-                    ->toggleable(isToggledHiddenByDefault: true),
-
-                IconColumn::make('published')
-                    ->label('Published')
-                    ->boolean()
-                    ->toggleable(),
+                TextColumn::make('id')->label('ID')->sortable()->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('title')->label('Title')->sortable()->searchable()->toggleable(),
+                TextColumn::make('slug')->label('Slug')->sortable()->searchable()->toggleable(),
+                TextColumn::make('category.name')->label('Category')->sortable()->searchable()->toggleable(),
+                ColorColumn::make('color')->toggleable(),
+                ImageColumn::make('image')->disk('public')->toggleable(),
+                TextColumn::make('created_at')->label('Created At')->dateTime()->sortable()->toggleable(),
+                TextColumn::make('tags')->label('Tags')->toggleable(isToggledHiddenByDefault: true),
+                IconColumn::make('published')->label('Published')->boolean()->toggleable(),
             ])
             ->defaultSort('created_at', 'asc')
             ->filters([
                 Filter::make('created_at')
                     ->label('Creation Date')
-                    ->schema([
-                        DatePicker::make('created_at')
-                            ->label('Select Date:'),
-                    ])
+                    ->schema([ DatePicker::make('created_at')->label('Select Date:'), ])
                     ->query(function (Builder $query, array $data): Builder {
-                        return $query->when(
-                            $data['created_at'],
-                            fn (Builder $query, $date): Builder => $query->whereDate('created_at', $date)
-                        );
+                        return $query->when($data['created_at'], fn (Builder $query, $date): Builder => $query->whereDate('created_at', $date));
                     }),
+                SelectFilter::make('category_id')->label('Category')->relationship('category', 'name')->preload(),
+            ])
+            ->recordActions([
+                ReplicateAction::make(),
+                
+                EditAction::make(),
+                
+                DeleteAction::make(),
 
-                SelectFilter::make('category_id')
-                    ->label('Category')
-                    ->relationship('category', 'name')
-                    ->preload(),
+                Action::make('status')
+                    ->label('Status Change')
+                    ->icon('heroicon-o-check-circle')
+                    ->requiresConfirmation() 
+                    ->schema([
+                        Checkbox::make('published')
+                            ->default(fn($record): bool => (bool) $record->published),
+                    ])
+                    ->action(function ($record, array $data) {
+                        $record->update(['published' => $data['published']]);
+                    }),
             ]);
     }
 }
